6af2766cf1573ab542af8f62ff783dc28c8b3756
David <audidave@gmail.com>
2014-11-15 12:28:11 +0100

    New object-oriented approach

--- KataBankOCR/BankOCR.cs
+++ KataBankOCR/BankOCR.cs
@@ -9,65 +9,115 @@ namespace KataBankOCR
 {
     public class BankOCR
     {
-      const short ENTRY_LENGTH = 3;
-      const short LINE_LENGTH = 27;
-      const short ENTRY_LINES = 4;
-      const short NO_ELEMENTS = 9;
+      
+      const short LINE_LENGTH = Entry.DIGIT_MAX * Entry.DIGIT_LENGTH;
+      const short ENTRY_LINES = 3; //each entry is n lines
+      
 
       public static void ReadFile(string filename)
-      {
-        string[][] tmp = new string[NO_ELEMENTS][];
+      {        
         string fileContent = File.ReadAllText(filename);
 
-        var tmp2 = GetDigit(fileContent);
+        IList<Entry> tmp2 = GetDigit(fileContent);
         
         
       }
 
-      private static IDictionary<short, string> GetDigit(string fileContent)
+      private static IList<Entry> GetDigit(string fileContent)
       {
-        IDictionary<short, string> tmp =
-          new Dictionary<short, string>();
-
+        IList<Entry> entries = new List<Entry>();
+        Entry currE = null; //current entry
+        string[][] currD = null;
+        short currELine = 0;
+        int line = 0;
 
         StringReader sr = new StringReader(fileContent);
-        string tmpLine = string.Empty;
-        
+        string tmpLine = sr.ReadLine();        
         
         while (tmpLine != null)
-        {
-          short digitIndex = 0;
-          tmpLine = sr.ReadLine();
-          if (tmpLine == null)
-            break;
-          if(!tmp.ContainsKey(digitIndex))
-            tmp.Add(new KeyValuePair<short,string>(digitIndex, string.Empty));
-          
-          for (int i = 0; i < LINE_LENGTH; i++)
+        {          
+          //Check if it's a new entry
+          if (line % ENTRY_LINES == 0)
+          {            
+            currD = new string[Entry.DIGIT_MAX][]; //0-8
+            currELine = 0;
+          }
+
+
+          //iterate all digits
+          for (short i = 0; i < Entry.DIGIT_MAX; i++)
           {
-            if (i > 0 && i % ENTRY_LINES == 0)
-              continue;
+            if (currD[i] == null)
+              currD[i] = new string[ENTRY_LINES];
+            currD[i][currELine] = GetDigitFromLine(tmpLine, (short)(i + 1));
+          }
 
-            string tmpS = tmpLine.Substring(i, 1);
-            if (string.IsNullOrEmpty(tmpS) || string.IsNullOrWhiteSpace(tmpS))
-              tmp[digitIndex] += "0";
-            else
-              tmp[digitIndex] += "1";
+          line++;
+          currELine++;
 
-            if (i > 0 && i % ENTRY_LENGTH - 1 == 0)
+          //If end of entry, add it
+          if (line > 0 && line % ENTRY_LINES == 0)
+          {            
+            Entry ent = new Entry();
+            short i = 0;
+            short number = 0;
+            foreach (string[] digit in currD)
             {
-              digitIndex++;
-              if (!tmp.ContainsKey(digitIndex))
-                tmp.Add(new KeyValuePair<short, string>(digitIndex, string.Empty));
-            }
+              if (Entry.TryConvertBitValue(GetDigitBitValue(digit), out number))
+                ent.Digits[i] = number;
+              i++;
+            }            
+          }
             
-              
-            
-          }          
+
+          tmpLine = sr.ReadLine();          
         }
 
+        return entries;
+      }
 
-        return tmp;
+      /// <summary>
+      /// Returns digit from one line
+      /// </summary>
+      /// <param name="line"></param>
+      /// <param name="digit">1-9</param>
+      /// <returns></returns>
+      private static string GetDigitFromLine(string line, short digit)
+      {
+        if (digit < Entry.DIGIT_MIN || digit > Entry.DIGIT_MAX)
+          throw new InvalidOperationException();
+
+        return line.Substring((Entry.DIGIT_LENGTH * (digit - 1)), Entry.DIGIT_LENGTH);
+      }
+
+      /// <summary>
+      /// Returns digit value from one line
+      /// </summary>
+      /// <param name="digit"></param>
+      /// <returns></returns>
+      private static short GetDigitBitValue(string digit)
+      {
+        short retValue = 0;
+        foreach (char c in digit.ToCharArray())
+        {
+          if (!char.IsWhiteSpace(c))
+            retValue++;
+        }
+
+        return retValue;
+      }
+
+      private static short GetDigitBitValue(string[] digit)
+      {
+        short retValue = 0;
+
+        //Iterate all lines for the digit
+        foreach (string sLine in digit)
+        {
+          retValue += GetDigitBitValue(sLine);
+        }
+
+        return retValue;
       }
     }
 }
---
+++ KataBankOCR/Entry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataBankOCR
+{
+  public class Entry
+  {
+    public const short DIGIT_LENGTH = 3;
+    public const short DIGIT_MIN = 1;
+    public const short DIGIT_MAX = 9;
+
+    private short[] _digits = new short[DIGIT_MAX];
+
+    /// <summary>
+    /// 9 digits
+    /// Pos 0 - 8
+    /// </summary>
+    public short[] Digits
+    {
+      get { return _digits; }
+    }
+
+    #region Utils
+
+    public static bool TryConvertBitValue(short bitValue, out short number)
+    {
+      number = 0;
+
+      switch (bitValue)
+      {
+        case 6:
+          number = 0;
+          break;
+
+        default:
+          return false;
+      }
+
+      return true;
+    }
+
+    
+
+    #endregion
+
+
+  }
+}
